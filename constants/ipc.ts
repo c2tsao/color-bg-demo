3ca58cac4207a5a126da816cae72d36f056e5674
c2tsao <malumtsao@gmail.com>
2025-03-18 13:36:14 +0800

    update ipc keys

--- constants/ipc.ts
+++ constants/ipc.ts
@@ -1,9 +1,9 @@
-export const IPC_WINDOW_READY_TO_SHOW = 'ready-to-show'
+export const IPC_WINDOW_READY_TO_SHOW = '(ipc)ready-to-show'
 
-export const IPC_CONFIG_STORE_GET = 'configStore:get'
-export const IPC_CONFIG_STORE_SET = 'configStore:set'
+export const IPC_CONFIG_STORE_GET = '(ipc)config-store:get'
+export const IPC_CONFIG_STORE_SET = '(ipc)config-store:set'
 
-export const IPC_FILE_IO_OPEN_DIALOG = 'openFileDialog'
-export const IPC_FILE_IO_GET_APP_DATA_PATH = 'getAppDataPath'
-export const IPC_FILE_IO_READ_APP_DATA_FILE = 'readAppDataFile'
-export const IPC_FILE_IO_READ_FILE = 'readFile'
+export const IPC_FILE_IO_OPEN_DIALOG = '(ipc)open-file-dialog'
+export const IPC_FILE_IO_GET_APP_DATA_PATH = '(ipc)get-app-data-path'
+export const IPC_FILE_IO_READ_APP_DATA_FILE = '(ipc)read-app-data-file'
+export const IPC_FILE_IO_READ_FILE = '(ipc)read-file'
